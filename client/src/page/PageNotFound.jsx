ed4bc10ad4fba18d8538f8ecb12cbea0928bb46c
Phol <Pholibertlim@gmail.com>
2023-02-26 04:03:33 +0800

    fix: page not found color

--- client/src/page/PageNotFound.jsx
+++ client/src/page/PageNotFound.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const PageNotFound = () => {
   return (
     <div>
-      <h1>Page Not Found</h1>
+      <h1 className='text-white'>Page Not Found</h1>
       <Link to='/login'>Home</Link>
     </div>
   );
