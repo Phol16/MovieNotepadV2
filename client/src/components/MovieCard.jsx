a7ebfbc3deb0d6baae38318c866e0a20e70e7cdf
Phol <Pholibertlim@gmail.com>
2023-02-27 17:09:43 +0800

    fixed:something

--- client/src/components/MovieCard.jsx
+++ client/src/components/MovieCard.jsx
@@ -8,7 +8,7 @@ const FeaturedCard = ({ image, title, year, redirect }) => {
     <div className='flex flex-col items-center hover:scale-105 transition-all ease-in duration-[150ms]'>
       <img src={image} alt='poster' className='w-36 h-48 md:w-36 xl:w-44 xl:h-60 rounded-md ' />
       <section className='text-xs md:text-[15px] flex justify-center items-center max-w-[180px]'>
-        <p className='mr-1'>{title}</p>
+        <p className='mr-1 text-white'>{title}</p>
         {year.map((event, index) => {
                 return <span key={event} className='text-red-600'>{
                   year.length > 1 ? (
