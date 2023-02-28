8905321b8c193d5f60823eb87d307d7bd61b388a
Phol <Pholibertlim@gmail.com>
2023-03-01 04:00:36 +0800

    feat: loading

--- client/src/page/ConnectingPage.jsx
+++ client/src/page/ConnectingPage.jsx
@@ -1,11 +1,19 @@
-import { faCircleDot, faSpinner, faTruckLoading } from '@fortawesome/free-solid-svg-icons'
+import {  faSpinner } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
+import { useState } from 'react'
 
 const ConnectingPage = () => {
+  const [waited, setWaited] = useState(false)
+
+  setTimeout(()=>{
+    setWaited(true)
+  },[5000])
+
   return (
     <div className='text-white flex flex-col justify-center items-center min-h-screen'>
       <h1 className='text-white text-4xl md:text-5xl xl:text-6xl animate-bounce'>Movie<span className='text-red-600'>Notepad</span></h1>
+      {waited ? <p className=' text-xl p-2'>Please Wait. </p>:null}
       <section className='text-xl flex gap-1 justify-center items-center'>
       <FontAwesomeIcon icon={faSpinner} className='animate-spin'/>
       <p>Connecting...</p>
