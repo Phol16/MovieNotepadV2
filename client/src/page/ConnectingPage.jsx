56f9fa7f08d78922f7daf18f29347a030504ccae
Phol <Pholibertlim@gmail.com>
2023-03-01 20:34:05 +0800

    feat: loading details

--- client/src/page/ConnectingPage.jsx
+++ client/src/page/ConnectingPage.jsx
@@ -5,18 +5,26 @@ import { useState } from 'react'
 
 const ConnectingPage = () => {
   const [waited, setWaited] = useState(false)
+  const [author, setAuthor] = useState(false)
 
   setTimeout(()=>{
     setWaited(true)
   },[5000])
 
+  setTimeout(()=>{
+    setAuthor(true)
+  },[10000])
+
   return (
     <div className='text-white flex flex-col justify-center items-center min-h-screen'>
       <h1 className='text-white text-4xl md:text-5xl xl:text-6xl animate-bounce'>Movie<span className='text-red-600'>Notepad</span></h1>
-      {waited ? <p className=' text-xl p-2'>Please Wait. </p>:null}
       <section className='text-xl flex gap-1 justify-center items-center'>
       <FontAwesomeIcon icon={faSpinner} className='animate-spin'/>
       <p>Connecting...</p>
+      </section>
+      <section className='bg-black/60 p-2 rounded-lg flex flex-col items-center'>
+      {waited ? <p className=' text-xl p-2'>Please Wait.</p>:null}
+      {author ? <p>App Created By: <span className='text-red-600'>Phol</span> </p>:null}
       </section>
       </div>
   )
