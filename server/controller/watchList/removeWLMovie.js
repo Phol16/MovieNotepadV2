af77ed224b100b1babfbe55e46786326045cc78b
Phol <Pholibertlim@gmail.com>
2023-03-01 17:44:54 +0800

    fixed remove endpoint

--- server/controller/watchList/removeWLMovie.js
+++ server/controller/watchList/removeWLMovie.js
@@ -1,24 +1,28 @@
-import WatchList from "../../models/WatchListModel.js"
-const removeWLMovie = async(req, res)=>{
- const { id } = req.params
+import WatchList from '../../models/WatchListModel.js';
+const removeWLMovie = async (req, res) => {
+  const { id } = req.params;
+  console.log(id);
 
- try {
-  const found = await WatchList.findOne({_id: id, deletedAt:null,userId:req.userId.id})
+  try {
+    const found = await WatchList.findOne({ _id: id, deletedAt: null, userId: req.user.id });
 
-  if(!found){
-    return res.status(404).json({
-      status:'success',
-      message:'Movie not found'
-    })
+    if (!found) {
+      return res.status(404).json({
+        status: 'success',
+        message: 'Movie not found',
+      });
+    }
+
+    await WatchList.findByIdAndUpdate(id, { deletedAt: Date.now() });
+    res.status(200).json({
+      status: 'success',
+      message: 'has been deleted',
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: 'failed',
+      mesasge: error.message,
+    });
   }
-
-  await WatchList.findByIdAndUpdate(id,{deletedAt:null})
-  res.status(200).json({
-    status:'success',
-    message:'has been deleted'
-  })
- } catch (error) {
-  
- }
-}
-export default removeWLMovie
+};
+export default removeWLMovie;
