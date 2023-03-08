664432b5fb8b537b377af9581c381621367e8db7
Phol <Pholibertlim@gmail.com>
2023-03-08 20:50:02 +0800

    feat: added delete notes feature

--- client/src/components/Notes.jsx
+++ client/src/components/Notes.jsx
@@ -1,11 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useEffect, useRef, useState } from 'react';
 import api from '../api/localhost';
 
 const Notes = ({ movieId, added }) => {
   const [notes, setNotes] = useState([]);
   const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(false);
+  const [delInfo, setDelInfo] = useState(false);
+
   const accessToken = localStorage.getItem('Token');
-  console.log(added);
   useEffect(() => {
     const fetchNotes = async () => {
       try {
@@ -17,14 +21,29 @@ const Notes = ({ movieId, added }) => {
             Authorization: `bearer ${accessToken}`,
           },
         }).then((res) => res.json());
-        console.log(response);
         response.status === 'failed' ? setError(response.message) : setNotes(response.data);
       } catch (error) {
         console.log(error.message);
       }
     };
     fetchNotes();
-  }, [added]);
+  }, [added,success]);
+
+  const handleDelete = async(note)=>{
+    try {
+      const response = await fetch(`${api()}/notes?id=${note}`, {
+        method: 'DELETE',
+        cors: 'cors',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `bearer ${accessToken}`,
+        },
+      }).then((res) => res.json());
+      setSuccess(!success)
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
 
   return (
     <div>
@@ -34,7 +53,24 @@ const Notes = ({ movieId, added }) => {
           <div className='grid grid-cols-2 lg:grid-cols-4 gap-1 p-2'>
             {notes.map((note) => {
               return (
-                <div key={note._id} className=''>
+                <div key={note._id}>
+                  <section className='flex justify-end'>
+                    <div className='relative px-3 flex flex-col'>
+                      <p className={`absolute text-xs -top-3 ${delInfo === note._id ? 'flex' : 'hidden'} self-center`}>Delete</p>
+                      <button
+                        onMouseOut={() => {
+                          setDelInfo(false);
+                        }}
+                        onMouseOver={() => {
+                          setDelInfo(note._id);
+                        }}
+                        onClick={()=>{handleDelete(note._id)}}
+                        className='bg-transparent text-white'
+                      >
+                        <FontAwesomeIcon icon={faTrash} />
+                      </button>
+                    </div>
+                  </section>
                   <section className=' bg-white text-black h-32 p-2 rounded-md text-sm'>
                     <h1 className='border-b'>{note.title}</h1>
                     <p className='overflow-auto h-24'>{note.content}</p>
