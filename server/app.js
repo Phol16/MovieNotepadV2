7547e64677df5f09c86435c7f75ea47609dadd45
Phol <Pholibertlim@gmail.com>
2023-03-11 03:59:55 +0800

    increased limit size

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ dotenv.config();
 
 const app = express()
 
-app.use(express.json({limit: '5MB'}))
+app.use(express.json({limit: '10MB'}))
 app.use(helmet())
 app.use(cors())
 app.use(morgan('dev'))
