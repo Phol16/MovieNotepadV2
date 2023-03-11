6cad3583a1598fc2dca3c8af57e3e3ce83391c7a
Phol <Pholibertlim@gmail.com>
2023-03-11 17:24:44 +0800

    feat: added cloudinary in movie update enpoint

--- server/controller/movies/updateMovie.js
+++ server/controller/movies/updateMovie.js
@@ -1,8 +1,11 @@
+import app from "../../app.js";
 import Movie from "../../models/MovieModel.js"
+import CloudinaryService from "../../utils/CloudinaryService.js";
 
 const updateMovie = async(req, res)=>{
   const {title, year, image, genre, imdbId, description} = req.body
   const {id} = req.query
+
 
   try {
     const findMovie = await Movie.findOne({_id: id, deletedAt:null});
@@ -14,12 +17,23 @@ const updateMovie = async(req, res)=>{
         message:'Movie not found'
       })
     }
+
+    if(image !== findMovie.image){
+     const cloudinary = app.get(CloudinaryService.serviceName)
+
+     const uploadedRes = await cloudinary.uploader.upload(image, {
+      upload_preset: 'movieNotepad',
+    })
+    await Movie.findByIdAndUpdate(id, {title, year, image: uploadedRes.url, genre, imdbId, description})
+    }else{
     await Movie.findByIdAndUpdate(id, data)
+    }
 
     res.status(200).json({
       status:'success',
       message:`${title}: has been updated`
     })
+
   } catch (error) {
     res.status(500).json({
       status:'failed',
