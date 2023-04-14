789d182a282a91b9226b8b2b3d345ad95ea082c8
Phol <Pholibertlim@gmail.com>
2023-04-14 22:14:58 +0800

    added delete function in watchlist

--- client_V3/src/page/MovieWLPage.tsx
+++ client_V3/src/page/MovieWLPage.tsx
@@ -1,20 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 import { useNavigate, useParams } from 'react-router-dom';
 import { primButton, primText, subText } from '../style/theme';
 import logo from '../assets/logo.svg';
 import home from '../assets/home.svg';
+import trash from '../assets/trash.svg';
 import { API } from '../api/Api';
+import Button from '../components/Button';
+import SecondaryButton from '../components/SecondaryButton';
 
 const MovieWLPage = () => {
   const [movie, setMovie] = useState<Record<string, any>>();
+  const [openDelete, setOpenDelete] = useState<boolean>(false);
+  const user = sessionStorage.getItem('user')
   const navigate = useNavigate();
-  const { WL } = useParams()
-console.log(WL)
+  const { WL } = useParams();
+
   useEffect(() => {
     const fetchMovie = async () => {
-      const response = await fetch(`${API}/movie/${WL}`, {
+      const response = await fetch(`${API}/watchList/movie/${WL}`, {
         method: 'GET',
         credentials: 'include',
         headers: {
@@ -28,26 +33,73 @@ console.log(WL)
     fetchMovie();
   }, []);
 
-  const handleImdb = () => {};
+  const handleImdb = () => {
+    if (movie) {
+      window.open(`https://www.imdb.com/title/${movie.movieId.imdbId}`, '_blank');
+    }
+  };
+
+  const handleDelete = () => {
+    const fetchDelete = async ()=>{
+      if(movie){
+        console.log('active')
+      const response = await fetch(`${API}/watchList/movie/${movie._id}`,{
+        method:'DELETE',
+        credentials:'include',
+        headers:{
+          'Content-Type': 'applicaiton/json',
+          authorization: `bearer ${user}`
+        }
+      }).then((res)=>res.json())
+      if(response.message === 'delete success'){
+        navigate('/home/watchList')
+      }
+    }
+    }
+    fetchDelete()
+  }
 
   return (
     <div className='max-w-6xl m-auto pt-20 px-3'>
       {movie ? (
         <div className='flex flex-col gap-2'>
-          <video src={movie.trailer} poster={movie.image} autoPlay loop muted className=' aspect-video m-auto max-h-[650px] overflow-hidden' />
+          <video src={movie.movieId.trailer} poster={movie.movieId.image} autoPlay loop muted className=' aspect-video m-auto max-h-[650px] overflow-hidden' />
           <main className='flex flex-col gap-2 max-w-lg m-auto'>
             <header className='flex items-center'>
-              <h1 className={`${primText} font-semibold text-secondary`}>{movie.title}</h1>
+              <h1 className={`${primText} font-semibold text-secondary`}>{movie.movieId.title}</h1>
               <LazyLoadImage src={logo} alt='Icon' className={`w-8 h-8`} />
             </header>
-            <h2>
-              / {movie.year} / {movie.genre.join(' ')}
-            </h2>
-
+            <section className='flex items-center justify-between'>
+              <h2>
+                / {movie.movieId.year} / {movie.movieId.genre.join(' ')}
+              </h2>
+              <aside className='relative flex items-center'>
+                {openDelete && (
+                  <section className=' rounded-lg absolute -left-[200px] gap-3 w-48 p-2 text-sm bg-white max-w-sm flex flex-col '>
+                    <h1 className='text-black'>Are you sure you want to delete {movie.movieId.title}?</h1>
+                    <Button Name={'Confirm'} handleClick={handleDelete} />
+                    <SecondaryButton
+                      Name={'Cancel'}
+                      handleClick={() => {
+                        setOpenDelete(false);
+                      }}
+                    />
+                  </section>
+                )}
+                <button
+                  title='delete'
+                  onClick={() => {
+                    setOpenDelete(!openDelete);
+                  }}
+                >
+                  <img src={trash} alt='Icon' className='w-8 h-8' />
+                </button>
+              </aside>
+            </section>
             <button className={`${primButton} ${subText}`} onClick={handleImdb}>
               Learn More
             </button>
-            <p className='max-w-md'>{movie.description}</p>
+            <p className='max-w-md'>{movie.movieId.description}</p>
           </main>
         </div>
       ) : (
