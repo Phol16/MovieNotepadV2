5e903779c6bbe04e85414fcd5bf5c85841205074
Phol <Pholibertlim@gmail.com>
2023-04-15 13:24:51 +0800

    changed api link

--- client/src/api/Api.ts
+++ client/src/api/Api.ts
@@ -1,3 +1,3 @@
 // export const API = 'https://movienotepadserver.onrender.com';
-export const API = 'http://localhost:8000';
-// export const API = 'https://movie-notepad-v3-server-crnu635wg-phol16.vercel.app';
+// export const API = 'http://localhost:8000';
+export const API = 'https://movienotepadserver-v3.vercel.app/';
