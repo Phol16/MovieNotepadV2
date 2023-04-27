b0c733b52404091b372bfa20f842d50d42597768
Phol <Pholibertlim@gmail.com>
2023-04-27 19:55:00 +0800

    hopefully cookie issue fixed

--- server/src/controllers/users/authentication.ts
+++ server/src/controllers/users/authentication.ts
@@ -105,6 +105,7 @@ export const logIn = async (req: Request, res: Response) => {
 
 export const logOut = async(req:Request, res:Response)=>{
   try {
+    res.cookie('MN_sessionToken', ' ',{httpOnly:env==='Development', secure:true, sameSite:'none'});
     res.clearCookie('MN_sessionToken',{ httpOnly:env==='Development', secure:true, sameSite:'none'})
     res.status(200).json({message:'log out success'})
   } catch (error) {
